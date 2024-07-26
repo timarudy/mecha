03caeb4bbff8835f23dd7dee62616b734d72eaac
Tymur Rudenko <googlrplaynh1@gmail.com>
2024-07-27 00:36:05 +0300

    confirmation added

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
     port: process.env.PORT,
     mongoURI: process.env.MONGO_URI,
+    email: process.env.AUTH_EMAIL,
+    pass: process.env.AUTH_PASS,
 };
--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -4,117 +4,218 @@ const Account = mongoose.model('accounts');
 const validator = require('validator');
 const argon2i = require('argon2-ffi').argon2i;
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+
+const UserOTPVerification = require("../model/userOTPVerification");
+const nodemailer = require('nodemailer');
+const keys = require('../config/keys');
 
 const passwordRegex = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,24})');
 
+let transporter = nodemailer.createTransport({
+    host: "smtp.ukr.net",
+    port: 465,
+    secure: true,
+    auth: {
+        user: keys.email,
+        pass: keys.pass,
+    },
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error.message);
+    }
+});
+
+// Required modules and initial setup omitted for brevity
+
 module.exports = app => {
-    // Routes
     app.post('/account/login', async (req, res) => {
-        const { username, password } = req.body;
+        try {
+            const { username, password } = req.body;
+            let response = {};
 
-        var response = {};
+            if (!username || !passwordRegex.test(password)) {
+                response.status = 0;
+                response.msg = "Invalid credentials";
+                return res.json(response);
+            }
 
-        if (username == null || !passwordRegex.test(password)) {
-            response.status = 0;
-            response.msg = "Invalid credentials";
-            res.send(response);
-            return;
-        }
-
-        var userAccount = await Account.findOne({ username: username }, 'username isAdmin password');
-        if (userAccount != null) {
-            argon2i.verify(userAccount.password, password).then(async success => {
+            let userAccount = await Account.findOne({ username }, 'username isAdmin password email confirmed');
+            if (userAccount) {
+                const success = await argon2i.verify(userAccount.password, password);
                 if (success) {
                     userAccount.lastAuthentication = Date.now();
                     await userAccount.save();
 
                     response.status = 1;
                     response.msg = "Logged in";
-                    response.data = (({username, isAdmin}) => ({username, isAdmin}))(userAccount);
-                    res.send(response);
-                    return;
-                } else {
-                    response.status = 0;
-                    response.msg = "Invalid credentials";
-                    res.send(response);
-                    return;
+                    response.data = {
+                        username,
+                        isAdmin: userAccount.isAdmin,
+                        email: userAccount.email,
+                        confirmed: userAccount.confirmed
+                    };
+                    return res.json(response);
                 }
-            });
-        } else {
+            }
+
             response.status = 0;
             response.msg = "Invalid credentials";
-            res.send(response);
-            return;
+            res.json(response);
+        } catch (error) {
+            res.status(500).json({ status: 0, msg: 'Internal server error' });
+            console.error(error);
         }
     });
 
     app.post('/account/register', async (req, res) => {
-        const { username, password, email } = req.body;
+        try {
+            const { username, password, email } = req.body;
+            let response = {};
 
-        var response = {};
+            if (!username || username.length < 5 || username.length > 16) {
+                response.status = 0;
+                response.msg = "Invalid username";
+                return res.json(response);
+            }
 
-        if (username == null || username.length < 5 || username.length > 16) {
-            response.status = 0;
-            response.msg = "Invalid username";
-            res.send(response);
-            return;
-        }
+            if (!validator.isEmail(email)) {
+                response.status = 0;
+                response.msg = "Invalid email";
+                return res.json(response);
+            }
 
-        if (!validator.isEmail(email)) {
-            response.status = 0;
-            response.msg = "Invalid email";
-            res.send(response);
-            return;
-        }
+            if (!passwordRegex.test(password)) {
+                response.status = 0;
+                response.msg = "Unsafe password";
+                return res.json(response);
+            }
 
-        if (!passwordRegex.test(password)) {
-            response.status = 0;
-            response.msg = "Unsafe password";
-            res.send(response);
-            return;
-        }
+            let userAccount = await Account.findOne({ username }, '_id email');
+            if (!userAccount) {
+                crypto.randomBytes(32, async (err, salt) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({
+                            status: 0,
+                            msg: 'Internal server error'
+                        });
+                    }
 
-        var userAccount = await Account.findOne({ username: username }, '_id');
-        if (userAccount == null) {
-            console.log("Create new account");
+                    try {
+                        const hash = await argon2i.hash(password, salt);
+                        const newAccount = new Account({
+                            username,
+                            password: hash,
+                            email,
+                            salt,
+                            isAdmin: false,
+                            confirmed: false,
+                            lastAuthentication: Date.now(),
+                        });
 
-            // Generate a unique hashed password
-            crypto.randomBytes(32, function (err, salt) {
-                if (err) {
-                    console.log(err);
-                }
-
-                argon2i.hash(password, salt).then(async hash => {
-                    var newAccount = new Account({
-                        username: username,
-                        password: hash,
-                        email: email,
-                        salt: salt,
-
-                        lastAuthentication: Date.now(),
-                    });
-
-                    await newAccount.save();
-                    response.status = 1;
-                    response.msg = "Account created";
-                    response.data = (({username}) => ({username}))(newAccount);
-                    res.send(response);
-                    return;
+                        await newAccount.save();
+                        await sendOTPVerificationEmail(newAccount, res);
+                    } catch (error) {
+                        console.error(error);
+                        res.status(500).json({
+                            status: 0,
+                            msg: 'Internal server error'
+                        });
+                    }
                 });
-            });
-        } else {
-            if (userAccount.username == username) {
+            } else {
                 response.status = 2;
-                response.msg = "Username is already in use";
-                res.send(response);
+                response.msg = userAccount.username === username ? "Username is already in use" : "Email is already in use";
+                res.json(response);
             }
-            else if (userAccount.email == email) {
-                response.status = 2;
-                response.msg = "Email is already in use";
-                res.send(response);
-            }
-
-            return;
+        } catch (error) {
+            res.status(500).json({ status: 0, msg: 'Internal server error' });
+            console.error(error);
         }
     });
-}
+
+    app.post('/account/confirmation', async (req, res) => {
+        try {
+            let { userId, otp } = req.body;
+
+            if (!userId || !otp) {
+                throw new Error("Empty otp is not allowed");
+            } else {
+                const UserOTPVerificationRecords = await UserOTPVerification.find({
+                    userId,
+                });
+                if (UserOTPVerificationRecords.length <= 0) {
+                    throw new Error("Account record does not exist or it has been already confirmed");
+                } else {
+                    const { expiresAt } = UserOTPVerificationRecords[0];
+                    const hashedOTP = UserOTPVerificationRecords[0].otp;
+
+                    if (expiresAt < Date.now()) {
+                        await UserOTPVerification.deleteMany({ userId });
+                        throw new Error("Code has expired. Please request again.");
+                    } else {
+                        const validOTP = await bcrypt.compare(otp, hashedOTP);
+
+                        if (!validOTP) {
+                            throw new Error("Invalid code passed");
+                        } else {
+                            await Account.updateOne({ _id: userId }, { confirmed: true });
+                            UserOTPVerification.deleteMany({ userId });
+                            res.json({
+                                status: 1,
+                                msg: "Email confirmed",
+                            });
+                        }
+                    }
+                }
+            }
+        } catch (error) {
+            res.json({
+                status: 0,
+                msg: error.message,
+            });
+        }
+    });
+};
+
+const sendOTPVerificationEmail = async ({ _id, email, username }, res) => {
+    try {
+        const otp = `${Math.floor(Math.random() * 9000 + 1000)}`;
+
+        const mailOptions = {
+            from: keys.email,
+            to: email,
+            subject: "Verify Your Email",
+            html: `<p>Hey, ${username} enter <b>${otp}</b> in <b>mecha</b> to verify your email</p>`,
+        };
+
+        const hashedOTP = await bcrypt.hash(otp, 10);
+        const newOTPVerification = new UserOTPVerification({
+            userId: _id,
+            otp: hashedOTP,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 600000,
+        });
+
+        await newOTPVerification.save();
+        await transporter.sendMail(mailOptions);
+
+        let response = {};
+
+        response.status = 1;
+        response.msg = "Account created";
+        response.data = {
+            _id,
+            email,
+            username,
+        };
+
+        res.json(response);
+    } catch (error) {
+        console.error('Failed to send verification email:', error);
+        res.status(500).json({ status: "FAIL", message: 'Failed to send verification email' });
+    }
+};
